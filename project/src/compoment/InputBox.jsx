efee3146280e978be92094f8baeec91b67a25a08
shubham <shubhamyelekar0@gmail.com>
2024-10-28 19:17:29 +0530

    new input params

--- project/src/compoment/InputBox.jsx
+++ project/src/compoment/InputBox.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 
-const InputBox = ({label, amount, onAmountOption}) => {
+const InputBox = ({
+  label,
+   amount,
+   onAmountChange,
+   onCurrencyChange,
+   currencyOption = [],
+   selectCurrency,
+   amountDisabled = false,
+   currencyDisabled = false,
+  }) => {
   return (
     <div className ="flex items-center justify-center  gap-2 border border-e-slate-100">
-    <div className='p-4 flex gap-2'>
-      <label htmlFor="from" className=''>From</label>
-      <input type="number" placeholder='Amount' />
-    </div>
-    <div className='text-slate-300'>--------</div>
-    <div className='p-4 flex gap-2'>
-      <label htmlFor="from" className=''>Currency</label>
-      <select name="currency-option" id="">
-        <option value="usd">usd</option>
-      </select>
-    </div>
+      <div className='p-4 flex gap-2'>
+        <label htmlFor="from" className=''>{label}</label>
+        <input type="number" placeholder='Amount'
+        disabled={amountDisabled }
+        />
+      </div>
+      <div className='text-slate-300'>--------</div>
+      <div className='p-4 flex gap-2'>
+        <label htmlFor="from" className=''>Currency</label>
+        <select name="currency-option" id="">
+          <option value="usd">usd</option>
+        </select>
+      </div>
     </div>
   )
 }
